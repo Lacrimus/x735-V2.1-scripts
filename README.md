1f7483b4024c8152d5ea5ec2abea72ddbced05d7
ge28dil <ge28dil@mytum.de>
2022-04-07 12:52:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# x735-install.sh
+Execute this file to install the safe shutdown script for the geekworm x735 V2.1 board, 
+which shares its script with the x730 board.
+
+Specefic features of this fork:
+
+* Detect and power down any sdx / hdx devices
+* Register the x735pwr.sh script as a systemd service
---
+++ scripts/x735-V2.1-shutdown.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#X735 Shutdown through software
+
+GPIOCHIP=gpiochip0
+
+BUTTON_PIN=18
+
+#Set internal biases and pull the pins
+( PID=$BASHPID; gpioset -B pull-up -m signal gpiochip0 $BUTTON_PIN=1; )
+
+SLEEP=${1:-4}
+
+re=^[0-9.]+
+if ! [[ $SLEEP =~ $re ]] ; then
+   echo "error: sleep time not a number" >&2; exit 1
+fi
+
+# Detect all (S)ATA devices
+DEVICES=()
+
+PART=$(cat /proc/partitions)
+
+for x in {a..z}
+do
+   #SATA
+   if [[ $PART == *"sd$x"* ]]
+   then
+      DEVICES+=("sd$x")
+   fi
+   #ATA
+   if [[ $PART == *"hd$x"* ]]
+   then
+      DEVICES+=("hd$x")
+   fi
+done
+
+printf "Shutting down all SATA devices... ($DEVICES)"
+for d in $DEVICES
+do
+   sudo hdparm -Y /dev/$d &
+   /bin/sleep 0.1
+   printf "Device $d off \n"
+done
+
+echo "X735 Shutting down..."
+/bin/sleep $SLEEP
+
+#restore GPIO 18
+kill -s SIGTERM $PID
+gpioset -B pull-down gpiochip0 $BUTTON_PIN=0
