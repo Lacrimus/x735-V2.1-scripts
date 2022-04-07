852708bef531c227713e3b990a70c8893e38b53d
ge28dil <ge28dil@mytum.de>
2022-04-07 14:16:46 +0200

    Added missing semicolon

--- scripts/x735-V2.1-shutdown.sh
+++ scripts/x735-V2.1-shutdown.sh
@@ -36,4 +36,4 @@ done
 echo "X735 Shutting down..."
 
 #Set internal biases and pull the pin up and shut down. Pull the pin down again in case the command fails
-{ gpioset -B pull-up gpiochip0 $BUTTON_PIN=1 && sudo poweroff } || { gpioset -B pull-down gpiochip0 $BUTTON_PIN=0; tput setaf 1; printf "Aborting shutdown" ; tput sgr0; }
+{ gpioset -B pull-up gpiochip0 $BUTTON_PIN=1 && sudo poweroff; } || { gpioset -B pull-down gpiochip0 $BUTTON_PIN=0; tput setaf 1; printf "Aborting shutdown" ; tput sgr0; }
